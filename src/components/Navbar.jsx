e033436f66aa3d072ee329ecba58c239e2b3325d
opeyemibioku <opeyemibioku@gmail.com>
2024-10-22 13:55:21 +0100

    updated the navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import ContactUsBtn from "./Buttons";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navigation() {
   const location = useLocation();
@@ -44,24 +44,24 @@ export default function Navigation() {
         >
           <ul className="font-medium flex flex-col pt-10  md:w-80 justify-start md:justify-center md:items-start items-center h-[80vh] md:h-auto p-4 md:p-0 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li className="w-full text-center border-b border-t border-gray-100 md:border-0">
-              <a
-                href="/about"
+              <Link
+                to="/about"
                 className={`block py-2 px-3 text-white/100  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-buttonGreen md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ${isActive(
                   "/about"
                 )}`}
               >
                 About us
-              </a>
+              </Link>
             </li>
             <li className="w-full text-center border-b border-t border-gray-100 md:border-0 md:w-44">
-              <a
-                href="/products"
+              <Link
+                to="/products"
                 className={`block py-2 px-1  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-buttonGreen md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ${isActive(
                   "/products"
                 )}`}
               >
                 Our Products
-              </a>
+              </Link>
             </li>
             {/* <div className="w-full md:w-auto md:hidden justify-between m-3">
                             <ContactUsBtn label={"Contact Us"} />
